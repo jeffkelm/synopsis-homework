9202a93c8335a09f6368490ac7629918babc46ed
Jeff Kelm <jeff.t.kelm@gmail.com>
2016-04-14 20:26:22 -0400

    Added logback dependency.

---
+++ calculator/src/main/java/com/kelm/synopsis/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kelm.synopsis.calculator;
+
+/**
+ * The args list should contain two arguments (the second is optional):
+ * <ul>
+ * <li>the expression to be calculated
+ * <li> the log level (OPTIONAL) --> options: INFO/ERROR/DEBUG (defaults to INFO) 
+ * </ul> 
+ */
+public class Calculator {
+	public static void main(String[] args) {
+
+	}
+}
